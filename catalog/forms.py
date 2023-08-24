9841f5cefc13b70bf91ab665e3a1c82e682730f1
IlyaKovalchukKIS <mrlivedance@vk.com>
2023-08-24 14:09:48 +0300

    добавлена валидация для полей name и description на использования запрещенных слов

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,6 +3,26 @@ from catalog.models import Product
 
 
 class ProductForm(forms.ModelForm):
+    __forbidden_words = ['казино',
+                         'криптовалюта',
+                         'крипта',
+                         'биржа',
+                         'дешево',
+                         'бесплатно',
+                         'обман',
+                         'полиция',
+                         'радар']
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+        description = cleaned_data.get('description')
+
+        for word in self.__forbidden_words:
+            if word in name.lower() or word in description.lower():
+                raise forms.ValidationError(f'В названии или в описании использовано запрещенное слово')
+        return cleaned_data
