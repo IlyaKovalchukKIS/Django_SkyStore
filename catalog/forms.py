61d6e966e01ac4509dd3940fe786db8d4a7cf7b6
IlyaKovalchukKIS <mrlivedance@vk.com>
2023-08-24 14:49:13 +0300

    добавлена стилизация формы создания продуктов

--- catalog/forms.py
+++ catalog/forms.py
@@ -13,6 +13,11 @@ class ProductForm(forms.ModelForm):
                          'полиция',
                          'радар']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Product
         fields = '__all__'
